2728284f0f85f83a7c78dc7dfb843cebc188f2f7
이정섭 <dark6ro@naver.com>
2023-05-02 10:11:12 +0900

    Update database JPA Complete

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/model/AttractionInfo.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/model/AttractionInfo.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Entity
+@Table(name="attraction_info")
 public class AttractionInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,27 +19,29 @@ public class AttractionInfo {
     @Column(name="content_type_id")
     private Long typeId; // foreign key
     @Column(name="sido_code")
-    private int sidoCode; // foreign key
+    private String sidoCode; // foreign key
     @Column(name="gugun_code")
-    private int gugunCode; // foreign key
+    private String gugunCode; // foreign key
 
     // 여기까지 key 값
+    @Column(length=100)
     private String title;
+    @Column(length=100)
     private String addr1;
+    @Column(length=50)
     private String addr2;
-    @Column(name="zipcode")
+    @Column(name="zipcode", length=50)
     private String zipCode;
+    @Column(length=50)
     private String tel;
-    @Column(name="first_image")
+    @Column(name="first_image", length=200)
     private String firstImage;
-    @Column(name="first_image2")
+    @Column(name="first_image2", length=200)
     private String firstImage2;
     @Column(name="readcount")
     private int readCount;
-    @Builder.Default
-    private BigDecimal latitude;
-    @Builder.Default
-    private BigDecimal longitude;
+    private String latitude;
+    private String longitude;
     @Column(name="mlevel")
     private String mLevel;
 }
