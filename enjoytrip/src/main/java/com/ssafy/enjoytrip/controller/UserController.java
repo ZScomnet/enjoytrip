00ccae9947dd2f074a80e09f7f4f4bb7ca82785a
이정섭 <dark6ro@naver.com>
2023-05-02 14:10:19 +0900

    Repair data.sql

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/controller/UserController.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.ssafy.enjoytrip.controller;
 
+import com.ssafy.enjoytrip.dto.MemberDto;
 import com.ssafy.enjoytrip.model.User;
 import com.ssafy.enjoytrip.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,9 @@ public class UserController {
     public List<User> getAllUser(){
         return userService.getAllUser();
     }
+    @GetMapping("/{id}")
+    public MemberDto getMemberById(@PathVariable Long id){
+        return userService.getMemberInfoById(id);
+    }
+
 }
